eb5d8063369d322c9d6ecab38bbfe20a96fab698
Nwankwo Ernest Onyebuchi <nwankwoernest2020@gmail.com>
2023-11-04 17:21:53 +0100

    fixed a bug

--- config/db.js
+++ config/db.js
@@ -1,13 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const MONGO_URI = process.env.MONGO_URI;
 const connectDB = async () => {
   try {
-    mongoose.set("strictQuery", false);
-    const connection = await mongoose.connect(MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    });
+    mongoose.set('strictQuery', false);
+    const connection = await mongoose.connect(
+      process.env.MONGO_URI ||
+        'mongodb+srv://e2scoba:blockchain@cluster0.b8qy1lb.mongodb.net/house_linkup?retryWrites=true&w=majority',
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      }
+    );
     console.log(`MongoDB Connected: ${connection.connection.host}`);
   } catch (error) {
     console.log(`Error: ${error.message}`);
